88531293c2a3539343b75aa958298a6a9a115d27
w34kl1ng <selasordranyam@gmail.com>
2025-03-04 08:36:36 +0800

    march4-update-content
    updated content

--- src/app/page.js
+++ src/app/page.js
@@ -8,14 +8,14 @@ export default function Home() {
           <header className="lg:sticky lg:top-0 lg:flex lg:max-h-screen lg:w-1/2 lg:flex-col lg:justify-between lg:py-24">
             <div>
               <h1 className="text-4xl font-bold tracking-tight text-slate-200 sm:text-5xl">
-                <a href="/">Maynard Manyak</a>
+                <a href="/">Maynard Rey Rosales</a>
               </h1>
               <h2 className="mt-3 text-lg font-medium tracking-tight text-slate-200 sm:text-xl">
-                Senior Senior Senior
+                Associate Software Engineer
               </h2>
               <p className="mt-4 max-w-xs leading-normal">
                 I build pixel-perfect, engaging, and accessible digital
-                experiences.
+                experiences in website and mobile application platforms.
               </p>
               <nav
                 className="nav hidden lg:block"

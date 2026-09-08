1df654c8ea958367fddf3d50f7e4151e71e636ec
w34kl1ng <selasordranyam@gmail.com>
2026-09-08 21:23:51 +0800

    Update page.js
    test

--- src/app/page.js
+++ src/app/page.js
@@ -141,7 +141,7 @@ export default function Home() {
               </div>
               <div>
                 <p className="mb-4">
-                  I’m a developer passionate about crafting accessible, pixel-perfect user interfaces that blend thoughtful design with robust engineering. My favorite work lies at the intersection of design and development, creating experiences that not only look great but are meticulously built for performance and usability.
+                  1I’m a developer passionate about crafting accessible, pixel-perfect user interfaces that blend thoughtful design with robust engineering. My favorite work lies at the intersection of design and development, creating experiences that not only look great but are meticulously built for performance and usability.
                 </p>
                 <p className="mb-4">
                   Currently, I'm a Senior Front-End Engineer at Klaviyo, specializing in accessibility. I contribute to the creation and maintenance of UI components that power Klaviyo’s frontend, ensuring our platform meets web accessibility standards and best practices to deliver an inclusive user experience.
